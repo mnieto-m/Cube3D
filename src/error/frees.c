eb70ca2329b42954953e25ff8d03d3ba2d7e62ed
mnieto-m <majanoyieto@gmail.com>
2026-05-24 18:49:09 +0200

    update frees

--- src/error/frees.c
+++ src/error/frees.c
@@ -1,8 +1,36 @@
 #include "../Include/cube.h"
 
-void free_data(void *strtcutura)
+void free_map(char **map)
+	{
+		int i;
+		i = 0;
+		while (map[i])
+		{
+			if (map[i])
+				free(map[i]);
+			i++;
+		}
+		free(map);
+	}
+void free_path(t_path *path)
 {
-	//LOG_FUNC();
-	if(strtcutura)
-		free(strtcutura);
+		if (path->no)
+			free(path->no);
+		if (path->so)
+			free(path->so);
+		if (path->we)
+			free(path->we);
+		if (path->ea)
+			free(path->ea);
+		free(path);
+}
+void free_data(t_data *data)
+{
+	if (!data)
+		return;
+	if (data->path)
+		free_path(data->path);
+	if (data->map)
+		free_map(data->map);
+	free(data);
 }
